bab11b4a802244c17e4ef72b2e888cd76c295680
_nullbot <sonderblohm.dev@gmail.com>
2017-03-02 02:49:07 +0100

    [MOS] Introduction to Inverse Kinematics

--- Source/MasterOfShadows/MasterOfShadowsGameMode.cpp
+++ Source/MasterOfShadows/MasterOfShadowsGameMode.cpp
@@ -9,7 +9,7 @@ AMasterOfShadowsGameMode::AMasterOfShadowsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behavior/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/FirstPersonCharacter/Behavior/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
