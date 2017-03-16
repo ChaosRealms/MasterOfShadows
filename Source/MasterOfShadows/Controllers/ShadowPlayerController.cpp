68035dc7d3074f254e4563c83e4dfec66dcd72fb
_nullbot <sonderblohm.dev@gmail.com>
2017-03-16 02:58:26 +0100

    [MOS] Added an InGame Widget with a timer, health and the current ground No.

---
+++ Source/MasterOfShadows/Controllers/ShadowPlayerController.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MasterOfShadows.h"
+#include "ShadowPlayerController.h"
+
+
+
+
+/*void AShadowPlayerController::OnPlayerDeath_Implementation()
+{
+
+}*/
---
+++ Source/MasterOfShadows/Controllers/ShadowPlayerController.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "ShadowPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MASTEROFSHADOWS_API AShadowPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+protected:
+
+		//UFUNCTION(BlueprintNativeEvent, Category = "Death")
+		//void OnPlayerDeath_Implementation();
+	
+	
+};
